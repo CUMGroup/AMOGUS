7c19b7f4aeb8740370f1ccb23629657340614782
Alex Hagl <alex@hagl.de>
2022-11-14 21:33:36 +0100

    Added Auth Controller

---
+++ src/api/AMOGUS.Api/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AMOGUS.Core.Centralization.User;
+using AMOGUS.Core.Common.Interfaces.User;
+using AMOGUS.Core.Domain.Models.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMOGUS.Api.Controllers {
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : Controller {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService _authService) {
+            this._authService = _authService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]LoginApiModel model) {
+            var res = await _authService.LoginUserAsync(model);
+            return res.Result.Succeeded ? Ok(res) : Unauthorized(res);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody]RegisterApiModel model) {
+            var res = await _authService.RegisterUserAsync(model, UserRoles.User);
+            return res.Result.Succeeded ? Ok(res) : UnprocessableEntity(res);
+        }
+    }
+}
