c4bf16a9fa1052221bd5d3c438ebacebea9191d5
WachtelHD <115617918+WachtelHD@users.noreply.github.com>
2022-11-15 09:07:22 +0100

    added package and app module
    hope this helps :D

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ParallaxDirective } from './parallax.directive';
+import { AmongusParallaxComponent } from './amongus-parallax/amongus-parallax.component';
+import { TextParallaxComponent } from './text-parallax/text-parallax.component';
+import { SidewaysScrollComponent } from './sideways-scroll/sideways-scroll.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GraphsComponent } from './graphs/graphs.component';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import * as echarts from 'echarts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule} from "@angular/material/expansion";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParallaxDirective,
+    AmongusParallaxComponent,
+    TextParallaxComponent,
+    SidewaysScrollComponent,
+    LandingPageComponent,
+    GraphsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
+    MatCheckboxModule,
+    MatButtonModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
