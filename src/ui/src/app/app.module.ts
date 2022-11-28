c7c7233de22dcc3ed21c039100d4c79f81a0b629
WachtelHD <115617918+WachtelHD@users.noreply.github.com>
2022-11-28 13:06:33 +0100

    login and nav bar

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -1,19 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ParallaxDirective } from './parallax.directive';
+import { AmongusParallaxComponent } from './amongus-parallax/amongus-parallax.component';
+import { TextParallaxComponent } from './text-parallax/text-parallax.component';
+import { SidewaysScrollComponent } from './sideways-scroll/sideways-scroll.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GraphsComponent } from './graphs/graphs.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import * as echarts from 'echarts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import { MatExpansionModule} from "@angular/material/expansion";
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GameViewComponent } from './game-view/game-view.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
+    ParallaxDirective,
+    AmongusParallaxComponent,
+    TextParallaxComponent,
+    SidewaysScrollComponent,
+    LandingPageComponent,
+    GraphsComponent,
+    LoginRegisterComponent,
+    NavBarComponent,
+    GameViewComponent
   ],
   imports: [
     BrowserModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
+    MatCheckboxModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatCardModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/ui/src/app/modules/home/nav-bar/nav-bar.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import {gsap} from "gsap";
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+
+export class NavBarComponent implements OnInit {
+
+  expanded = false;
+
+  menuArray: any[];
+
+  constructor() {}
+
+  // tl:any = gsap.timeline({defaults:{duration:2}})
+
+  ngOnInit(): void {
+    // this.tl
+    //   .to(".item", {width:100, duration:0.3})
+    //   .to(".menu", {left:0, duration:1})
+
+    this.menuArray = [
+      {
+        text:"this",
+        path:"",
+      },
+      {
+        text:"should",
+        path:"",
+      },
+      {
+        text:"display",
+        path:"",
+      },
+      {
+        text:"stuff",
+        path:"",
+      },
+    ]
+  }
+
+  //needs work / simply proof of concept
+  expand(){
+    if(this.expanded){
+
+      gsap.to(".upper",  {attr: {d: "M10,2 L2,2"}, x: 0, ease:"InOut"});
+      gsap.to(".middle",  {autoAlpha: 1});
+      gsap.to(".lower",  {attr: {d: "M10,8 L2,8"}, x: 0, ease:"InOut"});
+
+      // this.tl.reverse()
+
+      gsap.timeline()
+        .to(".item", {width:0, duration:0.3})
+        .to(".menu", {left:-200, duration:1})
+
+      this.expanded = false;
+    }else{
+
+      gsap.to(".upper",  {attr: {d: "M8,2 L2,8"}, x: 1, ease:"InOut"});
+      gsap.to(".middle",  {autoAlpha: 0});
+      gsap.to(".lower",  {attr: {d: "M8,8 L2,2"}, x: 1, ease:"InOut"});
+
+      // this.tl.play()
+
+      gsap.timeline()
+        .to(".item", {width:100, duration:0.3})
+        .to(".menu", {left:0, duration:1})
+
+      this.expanded = true;
+    }
+  }
+
+  animate(name:string){
+    gsap.to("." + name, {width:200,duration:0.4})
+  }
+
+  reverseAnimation(name:string){
+    gsap.to("." + name, {width:0,duration:0.4})
+  }
+}
