d040ea3d2d8fa527378879d79e1e3fc8f687c675
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-12-03 03:03:00 +0100

    finished UserService

--- src/api/AMOGUS.Core/Common/Interfaces/Database/IApplicationDbContext.cs
+++ src/api/AMOGUS.Core/Common/Interfaces/Database/IApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿namespace AMOGUS.Core.Common.Interfaces.Database
+﻿using AMOGUS.Core.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMOGUS.Core.Common.Interfaces.Database
 {
     public interface IApplicationDbContext
     {
-
-        // TODO: DbSets definieren
-        // -> ER- Diagramm aufsetzen
+        public DbSet<UserStats> UserStats { get; set; }
+        public DbSet<UserMedal> UserMedals { get; set; }
+        public DbSet<GameSession> GameSessions { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
         Task<int> SaveChangesAsync();
--- src/api/AMOGUS.Infrastructure/Services/User/UserService.cs
+++ src/api/AMOGUS.Infrastructure/Services/User/UserService.cs
@@ -1,37 +1,59 @@
 ﻿using AMOGUS.Core.Common.Communication;
+using AMOGUS.Core.Common.Exceptions;
+using AMOGUS.Core.Common.Interfaces.Database;
 using AMOGUS.Core.Common.Interfaces.User;
 using AMOGUS.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace AMOGUS.Infrastructure.Services.User {
     internal class UserService : IUserService {
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationDbContext _dbContext;
 
-        public UserService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager) {
+        public UserService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, IApplicationDbContext dbContext) {
             _roleManager = roleManager;
             _userManager = userManager;
+            _dbContext = dbContext;
         }
 
-        public Task<Result> DeleteUserAsync(string userId) {
-            throw new NotImplementedException();
+        public async Task<Result> DeleteUserAsync(string userId) {
+            var medalsToDelete = _dbContext.UserMedals.Where(m => m.UserId.Equals(userId)).ToList();
+            _dbContext.UserMedals.RemoveRange(medalsToDelete);
+
+            var statsToDelete = _dbContext.UserStats.Where(us => us.UserId.Equals(userId)).ToList();
+            _dbContext.UserStats.RemoveRange(statsToDelete);
+
+            var sessionsToDelete = _dbContext.GameSessions.Where(gs => gs.UserId.Equals(userId)).ToList();
+            _dbContext.GameSessions.RemoveRange(sessionsToDelete);
+
+            try {
+                await _dbContext.SaveChangesAsync();
+            } catch (Exception ex) {
+                return Result.Failure("Failed deleting user.");
+            }
+
+            ApplicationUser user = await GetUserAsync(userId);
+            IdentityResult result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded) {
+                return Result.Failure(result.Errors.Select(e => (e.Code + ": " + e.Description)));
+            }
+            return Result.Success();
         }
 
         public async Task<ApplicationUser> GetUserAsync(string userId) {
             ApplicationUser user = await _userManager.FindByIdAsync(userId);
             if (user == null) {
-                throw new exce
+                throw new UserNotFoundException($"The user with the userID {userId} coudn't be found qwq.");
             }
             return user;
         }
 
-        public Task<bool> IsInRoleAsync(string userId, string role) {
-            throw new NotImplementedException();
+        public async Task<bool> IsInRoleAsync(string userId, string role) {
+            ApplicationUser user = await GetUserAsync(userId);
+
+            return await _userManager.IsInRoleAsync(user, role);
         }
     }
 }
