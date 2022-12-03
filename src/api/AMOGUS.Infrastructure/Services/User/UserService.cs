a867fc3c7e4cbd29f42b324d240cb11927aeeb03
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-12-03 01:15:39 +0100

    created UserService class

---
+++ src/api/AMOGUS.Infrastructure/Services/User/UserService.cs
@@ -0,0 +1,33 @@
+﻿using AMOGUS.Core.Common.Communication;
+using AMOGUS.Core.Common.Interfaces.User;
+using AMOGUS.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMOGUS.Infrastructure.Services.User {
+    internal class UserService : IUserService {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager) {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public Task<Result> DeleteUserAsync(string userId) {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetUserAsync(string userId) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(string userId, string role) {
+            throw new NotImplementedException();
+        }
+    }
+}
