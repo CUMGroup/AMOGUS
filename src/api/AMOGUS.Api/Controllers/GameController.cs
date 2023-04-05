0b9a7fc27e032aee841b568fffae7945bfd40847
Alex Hagl <alex@hagl.de>
2023-04-05 11:17:13 +0200

    Game Controlle yeeees

--- src/api/AMOGUS.Api/Controllers/GameController.cs
+++ src/api/AMOGUS.Api/Controllers/GameController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Web.Http;
+﻿using AMOGUS.Core.Common.Interfaces.Game;
+using AMOGUS.Core.Domain.Enums;
+using AMOGUS.Core.Domain.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AMOGUS.Api.Controllers {
 
     [Route("game")]
-    public class GameController : ApiController {
-        public IActionResult Index() {
-            return View();
+    [Authorize]
+    public class GameController : Controller {
+
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService) {
+            _gameService = gameService;
+        }
+
+        [Route("new")]
+        [HttpPost]
+        public IActionResult NewSession([FromBody]CategoryType category) {
+            var userId = GetUserId();
+            if(userId is null) {
+                return Forbid();
+            }
+            return Ok(_gameService.NewSession(category, userId));
+        }
+
+        [Route("end")]
+        [HttpPost]
+        public async Task<IActionResult> EndSession([FromBody]GameSession gameSession) {
+            var userId = GetUserId();
+            if(userId is null) {
+                return Forbid();
+            }
+            await _gameService.EndSessionAsync(gameSession, userId);
+            return Ok();
+        }
+
+        private string? GetUserId() {
+            return HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
     }
 }
