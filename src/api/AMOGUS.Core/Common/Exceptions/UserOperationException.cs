632cf262896731592d11f5a759932c15b1a29361
Alex <alex@hagl.de>
2023-04-13 17:00:56 +0200

    Added new Exceptions

---
+++ src/api/AMOGUS.Core/Common/Exceptions/UserOperationException.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace AMOGUS.Core.Common.Exceptions {
+    public class UserOperationException : Exception {
+        public UserOperationException() {
+        }
+
+        public UserOperationException(string? message) : base(message) {
+        }
+
+        public UserOperationException(string? message, Exception? innerException) : base(message, innerException) {
+        }
+
+        protected UserOperationException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
