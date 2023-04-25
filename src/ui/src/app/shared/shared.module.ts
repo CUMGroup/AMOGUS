5feed7b16bf04da9b7fa7f1bc62efc04692e5496
Alex <alex@hagl.de>
2023-04-25 17:30:41 +0200

    Fixed css

--- src/ui/src/app/shared/shared.module.ts
+++ src/ui/src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import {RouterModule} from "@angular/router";
+import { AmogusButtonComponent } from './components/amogus-button/amogus-button.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    AmogusButtonComponent
   ],
   exports: [
-    NavBarComponent
+    NavBarComponent,
+    AmogusButtonComponent
   ],
   imports: [
     CommonModule,
