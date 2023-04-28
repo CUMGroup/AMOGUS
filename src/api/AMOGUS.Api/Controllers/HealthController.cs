da72b3be9c46e385357dd652527d787912d40239
Alex <alex@hagl.de>
2023-04-28 17:50:20 +0200

    Added HealthCheck endpoint

---
+++ src/api/AMOGUS.Api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AMOGUS.Api.Controllers {
+
+    [ApiController]
+    [Route("health")]
+    public class HealthController : Controller {
+
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult HealthCheck() {
+            return Ok("Everything is healthy!");
+        }
+
+    }
+}
