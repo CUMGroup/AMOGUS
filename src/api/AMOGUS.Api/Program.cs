da89c188f2a2a69ae86233b0afce1d1ec4bd4961
Alex <alex@hagl.de>
2023-05-01 16:23:53 +0200

    DI Refactor

--- src/api/AMOGUS.Api/Program.cs
+++ src/api/AMOGUS.Api/Program.cs
@@ -1,62 +1,35 @@
+using AMOGUS.Api;
 using AMOGUS.Api.BackgroundWorker;
 using AMOGUS.Core;
-using AMOGUS.Core.Centralization.User;
-using AMOGUS.Core.Common.Interfaces.Database;
-using AMOGUS.Core.Common.Interfaces.User;
 using AMOGUS.Infrastructure;
 using AMOGUS.Validation;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add env vars
+builder.Configuration.AddJsonFile("appsettings.json").AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
 
+builder.Services.AddSwaggerServices();
+
+// Add all service packages
 builder.Services.AddValidators();
 builder.Services.AddDataServices(builder.Configuration, builder.Environment.IsDevelopment());
 builder.Services.AddCoreServices();
 
 builder.Services.AddHostedService<StreakUpdateScheduler>();
 
-builder.Configuration.AddJsonFile("appsettings.json").AddEnvironmentVariables();
 
 var app = builder.Build();
 
 app.UsePathBase("/api");
 
-using (var scope = app.Services.CreateScope()) {
-    var db = scope.ServiceProvider.GetRequiredService<IApplicationDbContext>();
-    await db.EnsureDatabaseAsync();
+await app.EnsureDatabaseOnStartupAsync();
 
-    var user = scope.ServiceProvider.GetRequiredService<IAuthService>();
-    await user.CreateRolesAsync<UserRoles>();
-}
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
