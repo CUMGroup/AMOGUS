6ac19d71049769e07338e2e244e6c9f003af3366
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-05-02 16:40:21 +0200

    added tests for DeleteQuestionByIdAsync

--- src/api/AMOGUS.UnitTests/TeacherServiceTests.cs
+++ src/api/AMOGUS.UnitTests/TeacherServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AMOGUS.Core.Common.Exceptions;
 using AMOGUS.Core.Common.Interfaces.Game;
 using AMOGUS.Core.Common.Interfaces.Security;
+using AMOGUS.Core.Common.Interfaces.Teacher;
 using AMOGUS.Core.Domain.Models.Entities;
 using AMOGUS.Core.Services.Gameplay;
 using AMOGUS.Core.Services.Teacher;
@@ -69,15 +70,89 @@ namespace AMOGUS.UnitTests {
         #endregion
 
         #region DeleteQuestionByIdAsync
-        // test if RecordNotFoundException when quest null
-        // test if IO exception is catched
-        // test if all other exception is catched
-        // test if deleted
+        [Fact]
+        public async Task DeleteQuestionByIdAsync_WhenGivenQuestionId_AndQuestionDoesNotExist_ReturnsException() {
+            var questionFileAccessorMock = CreateIQuestionFileAccessorMock();
+            questionFileAccessorMock
+                .Setup(x => x.GetAllQuestions())
+                .Returns(new List<Question>());
+
+            var teacherService = new TeacherService(questionFileAccessorMock.Object, CreateTokenFactoryMock().Object);
+            var result = await teacherService.DeleteQuestionByIdAsync("testId");
+
+            Assert.True(result.IsFaulted);
+            Assert.True(result.exception is RecordNotFoundException);
+        }
+
+        [Fact]
+        public async Task DeleteQuestionByIdAsync_WhenGivenQuestionId_AndQuestionFileNotAccessible_ReturnsException() {
+            var questionFileAccessorMock = CreateIQuestionFileAccessorMock();
+            questionFileAccessorMock
+                .Setup(x => x.GetAllQuestions())
+                .Returns(new List<Question>() {
+                    new Question() { QuestionId = "testId"}
+                });
+
+            questionFileAccessorMock
+                .Setup(x => x.SaveQuestionFilesAsync())
+                .Returns(() => throw new IOException());
+
+            var teacherService = new TeacherService(questionFileAccessorMock.Object, CreateTokenFactoryMock().Object);
+            var result = await teacherService.DeleteQuestionByIdAsync("testId");
+
+            Assert.True(result.IsFaulted);
+            Assert.True(result.exception is IOException);
+        }
+
+        [Fact]
+        public async Task DeleteQuestionByIdAsync_WhenGivenQuestionId_AndEverythingIsFine_ReturnsTrue_AndQuestionDeleted() {
+            var questionsList = new List<Question>() { 
+                new Question() {QuestionId = "testId"}
+            };
+
+            var questionFileAccessorMock = CreateIQuestionFileAccessorMock();
+            questionFileAccessorMock
+                .Setup(x => x.GetAllQuestions())
+                .Returns(questionsList);
+
+            var teacherService = new TeacherService(questionFileAccessorMock.Object, CreateTokenFactoryMock().Object);
+            var result = await teacherService.DeleteQuestionByIdAsync("testId");
+
+            Assert.True(result.IsSuccess);
+            Assert.True(result == true);
+            Assert.True(questionsList.Count == 0);
+        }
         #endregion
 
         #region GetQuestionById
-        // test if RecordNotFoundException when quest null
-        // test if question returned
+        [Fact]
+        public void GetQuestionById_WhenGivenQuestionId_AndQuestionDoesNotExist_ReturnsException() {
+            var questionFileAccessorMock = CreateIQuestionFileAccessorMock();
+            questionFileAccessorMock
+                .Setup(x => x.GetAllQuestions())
+                .Returns(new List<Question>());
+
+            var teacherService = new TeacherService(questionFileAccessorMock.Object, CreateTokenFactoryMock().Object);
+            var result = teacherService.GetQuestionById("testId");
+
+            Assert.True(result.IsFaulted);
+            Assert.True(result.exception is RecordNotFoundException);
+        }
+        [Fact]
+        public void GetQuestionById_WhenGivenQuestionId_AndQuestionDoesExist_ReturnsQuestion() {
+            var questionFileAccessorMock = CreateIQuestionFileAccessorMock();
+            questionFileAccessorMock
+                .Setup(x => x.GetAllQuestions())
+                .Returns(new List<Question>() {
+                    new Question(){ QuestionId = "testId" }
+                });
+
+            var teacherService = new TeacherService(questionFileAccessorMock.Object, CreateTokenFactoryMock().Object);
+            var result = teacherService.GetQuestionById("testId");
+
+            Assert.True(result.IsSuccess);
+            Assert.True(result.Value.QuestionId.Equals("testId"));
+        }
         #endregion
     }
 }
