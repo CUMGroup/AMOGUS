1c187d3625084bc9e415320f6f60f6feaab318b9
Alex Hagl <alex@hagl.de>
2023-05-02 13:43:45 +0200

    Code cleanup

--- src/api/AMOGUS.Core/Services/Gameplay/QuestionFileAccessor.cs
+++ src/api/AMOGUS.Core/Services/Gameplay/QuestionFileAccessor.cs
@@ -31,7 +31,7 @@ namespace AMOGUS.Core.Services.Gameplay {
         }
 
         public void ReloadQuestions() {
-            if(_logger.IsEnabled(LogLevel.Information)) {
+            if (_logger.IsEnabled(LogLevel.Information)) {
                 _logger.LogInformation("Loading questions from files at {dir}", _exercisePath);
             }
             _questions = new List<Question>();
@@ -42,10 +42,10 @@ namespace AMOGUS.Core.Services.Gameplay {
             foreach (var f in files) {
                 var convertedQuestions = JsonConvert.DeserializeObject<List<Question>>(File.ReadAllText(f));
 
-                if(_logger.IsEnabled(LogLevel.Information)) {
+                if (_logger.IsEnabled(LogLevel.Information)) {
                     _logger.LogInformation("Loaded question {q}", convertedQuestions?.Select(x => x.ToString() + "\n"));
                 }
-                foreach(var q in convertedQuestions!) {
+                foreach (var q in convertedQuestions!) {
                     _questions.Add(q);
                 }
             }
