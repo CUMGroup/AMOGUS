95fdca55e02c65d7673934d254386c3e67423d8a
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-05-03 18:09:35 +0200

    only one more test missing. This will be done in new Branch

--- src/api/AMOGUS.UnitTests/StatsServiceTests.cs
+++ src/api/AMOGUS.UnitTests/StatsServiceTests.cs
@@ -5,7 +5,9 @@ using AMOGUS.Core.Common.Interfaces.Repositories;
 using AMOGUS.Core.DataTransferObjects.User;
 using AMOGUS.Core.Domain.Models.Entities;
 using AMOGUS.Core.Services.Gameplay;
+using AMOGUS.Infrastructure.Identity;
 using FluentValidation;
+using FluentValidation.Results;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -114,17 +116,236 @@ namespace AMOGUS.UnitTests {
         #endregion
 
         #region UpdateUserStatsAsync(UserStats userStats)
-        // return false
-        // res = 0 (fehler)
-        // res > 0
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenUserStats_AndValidationFails_ReturnsFalse() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult(new List<ValidationFailure>() {
+                    new ValidationFailure("Test", "Test error lol")
+                }));
+
+            var statsService = new StatsService(CreateUserStatsRepositoryMock().Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            var result = await statsService.UpdateUserStatsAsync(new UserStats());
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenUserStats_UpdateInDbFails_ReturnsFalse() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(0);
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            var result = await statsService.UpdateUserStatsAsync(new UserStats());
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenUserStats_AndEverythingIsFine_ReturnsTrue() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(1);
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            var result = await statsService.UpdateUserStatsAsync(new UserStats());
+
+            Assert.True(result);
+        }
         #endregion
 
         #region UpdateUserStatsAsync(GameSession session, bool[] answers, ApplicationUser user)
         // return false
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenGameSessionAnswersAndUser_AndValidationFails_ReturnsFalse() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult(new List<ValidationFailure>() {
+                    new ValidationFailure("Test", "Test error lol")
+                }));
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.GetUserStatsIncludeUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(new UserStats());
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            bool[] answers = { true, false };
+
+            var result = await statsService.UpdateUserStatsAsync(new GameSession(), answers, new ApplicationUser());
+
+            Assert.False(result);
+        }
+
         // res = 0
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenGameSessionAnswersAndUser_UpdateInDbFails_ReturnsFalse() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(0);
+            userStatsRepositoryMock
+                .Setup(x => x.GetUserStatsIncludeUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(new UserStats());
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            bool[] answers = { true, false };
+
+            var result = await statsService.UpdateUserStatsAsync(new GameSession(), answers, new ApplicationUser());
+
+            Assert.False(result);
+        }
+
         // res > 0
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenGameSessionAnswersAndUser_AndEverythingIsFine_ReturnsTrue() {
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(1);
+            userStatsRepositoryMock
+                .Setup(x => x.GetUserStatsIncludeUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(new UserStats());
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            bool[] answers = { true, false };
+
+            var result = await statsService.UpdateUserStatsAsync(new GameSession(), answers, new ApplicationUser());
+
+            Assert.True(result);
+        }
+
         // stats are updated
-        // if not played so far, does streak increase?
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenGameSessionAnswersAndUser_AndEverythingIsFine_StatsAreUpdatedCorrectly() {
+            var testStats = new UserStats() {
+                UserId = "testId",
+                Level = 1,
+                CurrentStreak = 1,
+                OverallAnswered = 2,
+                CorrectAnswers = 0,
+                TotalTimePlayed = 5,
+                QuickestAnswer = 5,
+                SlowestAnswer = 10,
+                LongestStreak = 1
+            };
+            
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(1);
+            userStatsRepositoryMock
+                .Setup(x => x.GetUserStatsIncludeUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(testStats);
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            var testGameSession = new GameSession() {
+                SessionId = "testSession",
+                UserId = "testId",
+                Playtime = 5,
+                CorrectAnswersCount = 0,
+                QuickestAnswer = 12,
+                SlowestAnswer = 10,
+            };
+            bool[] answers = { false, false };
+
+            var result = await statsService.UpdateUserStatsAsync(testGameSession, answers, new ApplicationUser());
+
+            Assert.True(result);
+            Assert.True(testStats.TotalTimePlayed == 10);
+            Assert.True(testStats.CorrectAnswers == 0);
+            Assert.True(testStats.QuickestAnswer == 5);
+            Assert.True(testStats.SlowestAnswer == 10);
+        }
+
+        [Fact]
+        public async Task UpdateUserStatsAsync_WhenGivenGameSessionAnswersAndUser_AndEverythingIsFine_StatsAreUpdatedCorrectly2() {
+            var testStats = new UserStats() {
+                UserId = "testId",
+                User = new ApplicationUser(),
+                Level = 1,
+                CurrentStreak = 1,
+                OverallAnswered = 2,
+                CorrectAnswers = 0,
+                TotalTimePlayed = 5,
+                QuickestAnswer = 5,
+                SlowestAnswer = 10,
+                LongestStreak = 1
+            };
+
+            var statsValidatorMock = CreateValidatorMock();
+            statsValidatorMock
+                .Setup(x => x.Validate(It.IsAny<UserStats>()))
+                .Returns(new ValidationResult());
+
+            var userStatsRepositoryMock = CreateUserStatsRepositoryMock();
+            userStatsRepositoryMock
+                .Setup(x => x.UpdateUserStatsAsync(It.IsAny<UserStats>()))
+                .ReturnsAsync(1);
+            userStatsRepositoryMock
+                .Setup(x => x.GetUserStatsIncludeUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(testStats);
+
+            var statsService = new StatsService(userStatsRepositoryMock.Object, CreateGameSessionRepositoryMock().Object, CreateDateTimeMock().Object, statsValidatorMock.Object) { };
+
+            var testGameSession = new GameSession() {
+                SessionId = "testSession",
+                UserId = "testId",
+                Playtime = 5,
+                CorrectAnswersCount = 1,
+                QuickestAnswer = 2,
+                SlowestAnswer = 11,
+                Questions = new List<Question>() { new Question(), new Question() }
+            };
+            bool[] answers = { true, false };
+
+            var result = await statsService.UpdateUserStatsAsync(testGameSession, answers, new ApplicationUser());
+
+            Assert.True(result);
+            Assert.True(testStats.TotalTimePlayed == 10);
+            Assert.True(testStats.CorrectAnswers == 1);
+            Assert.True(testStats.QuickestAnswer == 2);
+            Assert.True(testStats.SlowestAnswer == 11);
+        }
+
+        // [TODO] if not played so far, does streak increase? [TODO]
         #endregion
     }
 }
