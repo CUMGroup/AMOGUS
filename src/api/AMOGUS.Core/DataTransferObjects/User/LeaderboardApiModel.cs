e40526e0d511f19dc3365d71cf0574df75b726ca
Alex <alex@hagl.de>
2023-05-08 17:03:36 +0200

    Added LeaderboardService

---
+++ src/api/AMOGUS.Core/DataTransferObjects/User/LeaderboardApiModel.cs
@@ -0,0 +1,30 @@
+﻿using AMOGUS.Core.Domain.Models.Entities;
+
+namespace AMOGUS.Core.DataTransferObjects.User {
+    public class LeaderboardApiModel {
+
+        public LeaderboardUserStreak[] LongestStreaks { get; set; }
+        public LeaderboardUserStreak[] CurrentStreaks { get; set; }
+
+        public LeaderboardUserCorrectRatio[] CorrectRatios { get; set; }
+
+        public LeaderboardApiModel(LeaderboardUserStreak[] LongestStreaks, LeaderboardUserStreak[] CurrentStreaks, LeaderboardUserCorrectRatio[] CorrectRatios) {
+            this.LongestStreaks = LongestStreaks;
+            this.CurrentStreaks = CurrentStreaks;
+            this.CorrectRatios = CorrectRatios;
+        }
+    }
+
+    public class LeaderboardUserStreak {
+
+        public string Username { get; set; } = string.Empty;
+        public int Streak { get; set; }
+
+    }
+
+    public class LeaderboardUserCorrectRatio {
+
+        public string Username { get; set; } = string.Empty;
+        public double CorrectRatio { get; set; }
+    }
+}
---
+++ src/api/AMOGUS.Core/Services/Information/LeaderboardService.cs
@@ -0,0 +1,45 @@
+﻿using AMOGUS.Core.Common.Interfaces.Database;
+using AMOGUS.Core.Common.Interfaces.Game;
+using AMOGUS.Core.Common.Interfaces.Repositories;
+using AMOGUS.Core.Common.Interfaces.User;
+using AMOGUS.Core.DataTransferObjects.User;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AMOGUS.UnitTests")]
+namespace AMOGUS.Core.Services.Information {
+    internal class LeaderboardService : ILeaderboardService {
+
+        private readonly IUserStatsRepository _userStatsRepository;
+
+        public LeaderboardService(IUserStatsRepository userStatsRepository) {
+            _userStatsRepository = userStatsRepository;
+        }
+
+        public async Task<LeaderboardApiModel> GetLeaderboardAsync() {
+            var longest = await _userStatsRepository.GetTopOrderedByAsync(5, e => e.LongestStreak);
+            var current = await _userStatsRepository.GetTopOrderedByAsync(5, e => e.CurrentStreak);
+            var correctRatio = await _userStatsRepository.GetTopOrderedByAsync(5, e => e.GetCorrectRatio());
+
+            return new LeaderboardApiModel(
+                LongestStreaks: longest.Select(x =>
+                    new LeaderboardUserStreak {
+                        Username = x.User?.UserName ?? string.Empty,
+                        Streak = x.LongestStreak
+                    }).ToArray(),
+                CurrentStreaks: current.Select(x =>
+                    new LeaderboardUserStreak {
+                        Username = x.User?.UserName ?? string.Empty,
+                        Streak = x.CurrentStreak
+                    }
+                ).ToArray(),
+                CorrectRatios: correctRatio.Select(x =>
+                    new LeaderboardUserCorrectRatio {
+                        Username = x.User?.UserName ?? string.Empty,
+                        CorrectRatio = x.GetCorrectRatio()
+                    }
+                ).ToArray()
+            );
+        }
+    }
+}
