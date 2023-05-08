2fb44bf2cc736bcbfd4a82773ed62826819b5a61
Alex <alex@hagl.de>
2023-05-08 17:13:13 +0200

    Added Leaderboards to Information Controller

---
+++ src/api/AMOGUS.Api/Controllers/InformationController.cs
@@ -0,0 +1,22 @@
+﻿using AMOGUS.Core.Common.Interfaces.Game;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMOGUS.Api.Controllers {
+
+    [ApiController]
+    [Route("information")]
+    public class InformationController : Controller {
+
+        private readonly ILeaderboardService _leaderboardService;
+
+        public InformationController(ILeaderboardService leaderboardService) {
+            _leaderboardService = leaderboardService;
+        }
+
+        [Route("leaderboards")]
+        public async Task<ActionResult> GetLeaderboards() {
+            var boards = await _leaderboardService.GetLeaderboardAsync();
+            return Ok(boards);
+        }
+    }
+}
