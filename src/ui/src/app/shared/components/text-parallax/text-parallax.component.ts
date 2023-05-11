64904fafa47112c1d0bb84cebd03debaaca49457
Nick Hübner <115617918+WachtelHD@users.noreply.github.com>
2023-05-11 15:37:55 +0200

    :D

--- src/ui/src/app/shared/components/text-parallax/text-parallax.component.ts
+++ src/ui/src/app/shared/components/text-parallax/text-parallax.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {Router} from "@angular/router";
+import {debounceTime} from "rxjs";
 
 
 @Component({
@@ -11,54 +13,58 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export class TextParallaxComponent implements OnInit, OnDestroy {
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
     this.initScrollTriggers();
   }
 
   ngOnDestroy(): void {
-
+    ScrollTrigger.getAll().forEach(t => t.kill())
   }
 
-  initScrollTriggers() {
+  async initScrollTriggers() {
     // when removing the revealUp css class has Opacity:0 -> hast to be changed when changing
-    gsap.registerPlugin(ScrollTrigger)
-    gsap.utils.toArray(".revealUp").forEach((elem:any) => {
-      ScrollTrigger.create({
-        trigger: elem,
-        start: "top 90%",
-        end: "bottom 10%",
-        onEnter: () => {
-          gsap.fromTo(
-            elem,
-            { y: 100, autoAlpha: 0 },
-            {
-              duration: 1.25,
-              y: 0,
-              autoAlpha: 1,
-              ease: "back",
-              overwrite: "auto"
-            }
-          );
-        },
-        onLeave: () => {
-          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-        },
-        onEnterBack: () => {
-          gsap.fromTo(
-            elem,
-            { y: -100, autoAlpha: 0 },
-            {
-              duration: 1.25,
-              y: 0,
-              autoAlpha: 1,
-              ease: "back",
-              overwrite: "auto"
-            }
-          );
-        },
-        onLeaveBack: () => {
-          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-        }
+
+    await new Promise(f => setTimeout(f, 0)).then(()=>{
+      gsap.registerPlugin(ScrollTrigger)
+      gsap.utils.toArray(".revealUp").forEach((elem: any) => {
+        ScrollTrigger.create({
+          trigger: elem,
+          start: "top 90%",
+          end: "bottom 10%",
+          markers: true,
+          onEnter: () => {
+            gsap.fromTo(
+              elem,
+              {y: 100, autoAlpha: 0},
+              {
+                duration: 1.25,
+                y: 0,
+                autoAlpha: 1,
+                ease: "back",
+                overwrite: "auto",
+              }
+            );
+          },
+          onLeave: () => {
+            gsap.fromTo(elem, {autoAlpha: 1}, {autoAlpha: 0, overwrite: "auto"});
+          },
+          onEnterBack: () => {
+            gsap.fromTo(
+              elem,
+              {y: -100, autoAlpha: 0},
+              {
+                duration: 1.25,
+                y: 0,
+                autoAlpha: 1,
+                ease: "back",
+                overwrite: "auto"
+              }
+            );
+          },
+          onLeaveBack: () => {
+            gsap.fromTo(elem, {autoAlpha: 1}, {autoAlpha: 0, overwrite: "auto"});
+          }
+        });
       });
     });
   }
