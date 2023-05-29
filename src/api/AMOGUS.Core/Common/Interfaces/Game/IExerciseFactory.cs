c89c985afa49ee7507f66d812569f49f5af94c36
Alex <alex@hagl.de>
2023-05-29 16:57:41 +0200

    Refactored MentalExerciseFactory

--- src/api/AMOGUS.Core/Common/Interfaces/Game/IExerciseFactory.cs
+++ src/api/AMOGUS.Core/Common/Interfaces/Game/IExerciseFactory.cs
@@ -1,17 +1,12 @@
 ﻿
 
 using AMOGUS.Core.Domain.Models.Entities;
-using AMOGUS.Core.Domain.Models.Generators;
 
 namespace AMOGUS.Core.Common.Interfaces.Game {
     public interface IExerciseFactory {
 
         Question GenerateRandomQuestion(bool insaneMode);
 
-        MentalExerciseModel GenerateRandomExerciseModel(bool insaneMode);
-
         string CalcAnswer(string question);
-
-        MentalExerciseModel CalcXp(MentalExerciseModel expr);
     }
 }
--- src/api/AMOGUS.Core/Factories/MentalExerciseFactory.cs
+++ src/api/AMOGUS.Core/Factories/MentalExerciseFactory.cs
@@ -13,6 +13,8 @@ namespace AMOGUS.Core.Factories {
 
     public class MentalExerciseFactory : IExerciseFactory {
 
+        private const int maxGenerationTryCount = 10;
+
         private static readonly Random _rng = new();
         private const int numOperandsMaxXp = 20;
         private const int avgOperandsMaxXp = 5;
@@ -25,7 +27,7 @@ namespace AMOGUS.Core.Factories {
         public Question GenerateRandomQuestion(bool insaneMode) {
 
             int tryCount = 0;
-            while (tryCount++ < 10) {
+            while (tryCount++ < maxGenerationTryCount) {
 
                 var expr = GenerateRandomExerciseModel(insaneMode);
                 var ansString = CalcAnswer(expr.Expression);
@@ -41,32 +43,19 @@ namespace AMOGUS.Core.Factories {
 
                 try {
                     expr = CalcXp(expr);
-                    if (expr.Difficulty == null)
+                    if (expr.Difficulty is null)
                         continue;
-                    if (expr.Xp == null)
+                    if (expr.Xp is null)
                         continue;
-                    return new Question {
-                        Answer = ans.ToString(),
-                        Category = insaneMode ? CategoryType.RANDOMMENTAL_INSANE : CategoryType.RANDOMMENTAL,
-                        Difficulty = (DifficultyType) expr.Difficulty!,
-                        Exercise = expr.Expression,
-                        ExperiencePoints = (int) expr.Xp!,
-                        QuestionId = "random"
-                    };
+
+                    return ExpressionModelToQuestion(expr, insaneMode);
                 }
                 catch (ArgumentException) {
                     continue;
                 }
             }
             // Should be pretty much unreachable
-            return new Question {
-                Answer = "37",
-                Category = insaneMode ? CategoryType.RANDOMMENTAL_INSANE : CategoryType.RANDOMMENTAL,
-                Difficulty = DifficultyType.EASY,
-                Exercise = "30 + 7",
-                ExperiencePoints = 4,
-                QuestionId = "random"
-            };
+            return DummyQuestion(insaneMode);
         }
 
         public string CalcAnswer(string question) {
@@ -90,11 +79,8 @@ namespace AMOGUS.Core.Factories {
             }
         }
 
-        [ExcludeFromCodeCoverage]
-        public MentalExerciseModel GenerateRandomExerciseModel(bool insaneMode = false) {
-            var numOperands = insaneMode ?
-        GenerateIntWithFallingProbability(3, 7)
-        : GenerateIntWithFallingProbability(2, 5);
+        private MentalExerciseModel GenerateRandomExerciseModel(bool insaneMode = false) {
+            var numOperands = GetRandomNumberOfOperands(insaneMode);
 
             var expr = new MentalExerciseModel {
                 Operands = new int[numOperands],
@@ -104,31 +90,21 @@ namespace AMOGUS.Core.Factories {
             };
 
             expr.Expression += expr.Operands[^numOperands] = _rng.Next(expr.MinOperand, expr.MaxOperand);
-            numOperands--;
+            --numOperands;
 
             while (numOperands > 0) {
 
-                var signRng = insaneMode ?
-                    _rng.Next(3)
-                    : GenerateIntWithFallingProbability(0, 3);
+                var signRng = GetRandomOperator(insaneMode);
 
                 expr.Operators[^numOperands] = signRng;
-
-                switch (signRng) {
-                    case 0:
-                        expr.Expression += " + ";
-                        break;
-                    case 1:
-                        expr.Expression += " - ";
-                        break;
-                    case 2:
-                        expr.Expression += " * ";
-                        break;
-                }
+                expr.Expression += OperatorToSignString(signRng);
+                
                 byte tryCount = 0;
                 do {
                     expr.Operands[^numOperands] = _rng.Next(expr.MinOperand, Math.Max(expr.MaxOperand - 20, 10));
                     ++tryCount;
+
+                // if insaneMode -> Try to reduce operands that are divisible by 10
                 } while (insaneMode && tryCount <= 3 && expr.Operands[^numOperands] % 10 == 0);
                 expr.Expression += expr.Operands[^numOperands];
                 numOperands--;
@@ -136,8 +112,52 @@ namespace AMOGUS.Core.Factories {
             return expr;
         }
 
-        [ExcludeFromCodeCoverage]
-        public MentalExerciseModel CalcXp(MentalExerciseModel expr) {
+        /** <summary>
+         * RandomMentalMode: between 2 and 4 operands<br></br>
+         * RandomMentalInsaneMode: between 3 and 6 operands
+         * </summary>
+         */
+        private int GetRandomNumberOfOperands(bool insaneMode) {
+            
+            return insaneMode ?
+                GenerateIntWithFallingProbability(3, 7)
+                : GenerateIntWithFallingProbability(2, 5);
+        }
+
+        /** <summary>
+         *  RandomMentalMode: +, -, * with falling probability <br></br>
+         *  RandomMentalInsaneMode: +, -, * with equal probability
+         * </summary>
+         */
+        private int GetRandomOperator(bool insaneMode) {
+            return insaneMode ?
+                    _rng.Next(3)
+                    : GenerateIntWithFallingProbability(0, 3);
+        }
+
+        /** <summary>
+         * Maps:<br></br>
+         * 0: ' + ' <br></br>
+         * 1: ' - ' <br></br>
+         * 2: ' * ' <br></br>
+         * default: ' / '
+         * </summary>
+         */
+        private string OperatorToSignString(int op) {
+            return op switch {
+                0 => " + ",
+                1 => " - ",
+                2 => " * ",
+                _ => " / ",
+            };
+        }
+
+        /**<summary>
+         * Xp-Calculation (latex):<br></br>
+         * \text{xp}=Max( \left(\frac{\begin{pmatrix} \text{numOperandsMaxXp} \\ \text{avgOperandsMaxXp} \\ \text{avgOperatorsMaxXp} \\ \text{answerMaxXp} \end{pmatrix}^\top \cdot \begin{pmatrix} \frac{\text{numOperands}-2}{6} \\ \frac{Median(\text{Operands})}{40} \\ \frac{Median(\text{Operators})}{2} \\ \frac{|\text{Answer}|}{10000} \end{pmatrix}}{Max((\text{amountMod5} - \text{amountNotMod5} - 1), 1) \cdot (\text{amountMod10} + 1)}\right), 0)
+         * </summary>
+         */
+        private MentalExerciseModel CalcXp(MentalExerciseModel expr) {
             if (expr.Answer == null) {
                 throw new ArgumentException("No answer given");
             }
@@ -145,35 +165,63 @@ namespace AMOGUS.Core.Factories {
                 throw new ArgumentException("Answer is too big");
             }
 
-            int amountMod10 = expr.Operands.Select(e => e % 10 == 0 ? 1 : 0).Sum() + 1;
-            int amountMod5 = expr.Operands.Select(e => e % 5 == 0 ? 1 : 0).Sum() + 1 - amountMod10;
+            int amountMod10 = CountElementsDivisibleBy(expr.Operands, 10);
+            // all mod 5s are also mod 10s -> so exclude the mod 10s
+            int amountMod5 = CountElementsDivisibleBy(expr.Operands, 5) - amountMod10;
+            int amountNotMod5 = expr.Operands.Length - amountMod5;
 
             double numOperandsNormalized = (expr.Operands.Length - 2) / 6d;
             double avgOperandsNormalized = Median(expr.Operands) / expr.MaxOperand;
             double avgOperatorsNormalized = Median(expr.Operators) / 2;
             double answerNormalized = Math.Abs((int) expr.Answer) / 10_000d;
 
-            expr.Xp = Math.Max((int) (((numOperandsMaxXp * numOperandsNormalized
+            double weightedParams = 
+                numOperandsMaxXp * numOperandsNormalized
                 + avgOperandsMaxXp * avgOperandsNormalized
                 + avgOperatorsMaxXp * avgOperatorsNormalized
-                + answerMaxXp * answerNormalized) / (double) Math.Max((amountMod5 - (expr.Operands.Length - amountMod5 + 1)), 1)) / (double) amountMod10), 0);
+                + answerMaxXp * answerNormalized;
+ 
+            double modWeight = 1d / Math.Max(amountMod5 - amountNotMod5 - 1, 1) * (amountMod10 + 1);
 
-            expr.Difficulty = Math.Max(Math.Min((int) Math.Round(-Math.Exp((-4d / sumMaxXp) * (int) expr.Xp + 1.4) + 4), 4), 0);
+            expr.Xp = Math.Max((int) ( weightedParams *  modWeight), 0);
+
+            expr.Difficulty = CalculateDifficultyForXp((int)expr.Xp);
 
             return expr;
         }
 
-        /*
-         * This generates a random number in the range [min, max) (exclusive)
-         * 
-         * It was tested with random numbers in [2, 6) and yielded following probability distribution. (100_000_000 tries)
-         * 
-         *  2 : 70% (70709718 Hits)
-         *  3 : 13% (13381646 Hits)
-         *  4 :  8% (8969867 Hits)
-         *  5 :  6% (6938769 Hits)
+        /**<summary>
+         * Counts the number of elements that are divisible by 10
+         * </summary>
          */
-        [ExcludeFromCodeCoverage]
+        internal int CountElementsDivisibleBy(IEnumerable<int> collection, int divisibleBy) {
+            return collection.Select(e => e % divisibleBy == 0 ? 1 : 0).Sum();
+        }
+
+        /** <summary>
+         * Calculates the Difficulty [0, 4] relative to a given xp amount.<br></br>
+         * Latex: clamp(\lfloor-e^{\frac{-4}{\text{sumMaxXp}}\cdot\text{xp}+1.4}+4\rceil, 0, 4) <br></br>
+         * Graph: https://www.desmos.com/calculator/tkgwnw0fkm
+         * </summary>
+         */
+        private int CalculateDifficultyForXp(int xp) {
+            double functionValue = - Math.Exp(-4d / sumMaxXp * xp + 1.4) + 4;
+            int roundedValue = (int)Math.Round(functionValue);
+            return Math.Clamp(roundedValue, 0, 4);
+        }
+
+        /**
+         * <summary>
+         * This generates a random number in the range [min, max) (exclusive).
+         * <br></br>
+         * It was tested with random numbers in [2, 6) and yielded following probability distribution. (100_000_000 tries)
+         * <br></br>
+         *  2 : 70% (70709718 Hits)<br></br>
+         *  3 : 13% (13381646 Hits)<br></br>
+         *  4 :  8% (8969867 Hits)<br></br>
+         *  5 :  6% (6938769 Hits)<br></br>
+         *  </summary>
+         */
         private static int GenerateIntWithFallingProbability(int min, int max) {
             // make max exclusive
             max -= 1;
@@ -203,6 +251,39 @@ namespace AMOGUS.Core.Factories {
             return (sortedArr[mid] + sortedArr[Math.Max(Math.Min(mid - 1, sortedArr.Length - 1), 0)]) / 2d;
         }
 
+        private static Question DummyQuestion(bool insaneMode) {
+            return new Question {
+                Answer = "37",
+                Category = insaneMode ? CategoryType.RANDOMMENTAL_INSANE : CategoryType.RANDOMMENTAL,
+                Difficulty = DifficultyType.EASY,
+                Exercise = "30 + 7",
+                ExperiencePoints = 4,
+                QuestionId = "random"
+            };
+        }
 
+        private static Question ExpressionModelToQuestion(MentalExerciseModel expr, bool insaneMode) {
+            if(expr is null) {
+                throw new ArgumentException("Expression cannot be null");
+            }
+            if(expr.Answer is null) {
+                throw new ArgumentException("Answer cannot be null");
+            }
+            if (expr.Xp is null) {
+                throw new ArgumentException("Xp cannot be null");
+            }
+            if (expr.Difficulty is null) {
+                throw new ArgumentException("Difficulty cannot be null");
+            }
+
+            return new Question {
+                Answer = ((int)expr.Answer).ToString(),
+                Category = insaneMode ? CategoryType.RANDOMMENTAL_INSANE : CategoryType.RANDOMMENTAL,
+                Difficulty = (DifficultyType) expr.Difficulty,
+                Exercise = expr.Expression,
+                ExperiencePoints = (int)expr.Xp,
+                QuestionId = "random"
+            };
+        }
     }
 }
